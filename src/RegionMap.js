29cace7d35a4143541634d8401af3cdd0be6cd83
Max Kviatkouski <Max.Kviatkouski@nordstrom.com>
2018-09-20 15:58:10 -0700

    add leaflet import and poc use

--- src/RegionMap.js
+++ src/RegionMap.js
@@ -1,10 +1,13 @@
 import './Map.css';
 import React, {Component} from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position = [51.505, -0.09]
 
 export default class RegionMap extends Component {
     render() {
         return(
-            <h2>I'm a map!</h2>
+            <Map center={position} zoom={13}/>
         );
     }
 }
